2451c9ca9e967deddeb506a9fa13b25e5c7f8db5
Abin-Isaac <35003281+Abin-Isaac@users.noreply.github.com>
2021-11-15 05:28:15 -0800

    Delete rustComments.rs

--- rustComments.rs
+++
@@ -1,7 +0,0 @@
-// Try Rust once.... Single Line 
-
-  /*
-Most fascinating thing is that programs never take down their standard, no matter how experienced or fresher the programmer is!  Multi line 
-*/
-
-/// Beware, This is a doc comment Take this seriously
