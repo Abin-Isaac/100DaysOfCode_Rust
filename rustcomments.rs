dcd92d52d807c19f08282b6e905962e1798b9d3b
Abin-Isaac <35003281+Abin-Isaac@users.noreply.github.com>
2021-11-15 05:28:47 -0800

    Add files via upload

---
+++ rustcomments.rs
@@ -0,0 +1,6 @@
+
+// Try Rust once....
+/*
+Most fascinating thing is that programs never take down their standard, no matter how experienced or fresher the programmer is!
+*/
+/// Beware, This is a doc comment Take this seriously 
