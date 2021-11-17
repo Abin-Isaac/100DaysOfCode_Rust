7ddac15d9a77cea0fa999bbb5b954bc85c606bc6
Abin-Isaac <35003281+Abin-Isaac@users.noreply.github.com>
2021-11-17 06:17:22 -0800

    Add files via upload

---
+++ RustVariables.rs
@@ -0,0 +1,10 @@
+fn main() {
+    //let a:i8 = 5777777; #an error as i8 only takes between range -128 to +128
+    let b = true;
+    let c = "Enter in to the Rest";
+    let d = 5.7;
+    //println!("The value of a is: {}", a+b);
+    println!("The value of b is: {}", b);
+    println!("The value of c is: {}", c);
+    println!("The value of d is: {}", d);
+}
